dfb0c668ead01674ffab090a2effa6b4a829c89c
unknown <nm_bensaci@esi.dz>
2024-12-21 19:59:05 +0100

    version

--- TC-gestion/src/App.jsx
+++ TC-gestion/src/App.jsx
@@ -1,15 +1,25 @@
+// filepath: /c:/Users/Redouane/Desktop/TC gestion/FrontEnd/TC-gestion/src/App.jsx
 /* eslint-disable no-unused-vars */
 import "./app.css";
 import AddNew from "./pages/AddNew";
-import {RouterProvider, createBrowserRouter, createRoutesFromElements, Route} from "react-router-dom"
+import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
 import Editpage from "./pages/Editpage";
 import Signup from "./pages/signup";
 import Profile from "./pages/Profile";
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/addnew" element={<AddNew />} />
+      <Route path="/editpage" element={<Editpage />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/profile" element={<Profile />} />
+    </>
+  )
+);
 const App = () => {
-
-  return (
-    <Profile />
+    return (
+    <RouterProvider router={router} />
   );
 };
 
