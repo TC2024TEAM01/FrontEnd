961285c2688178fcb5bd04b3c64f7cd9afc82de8
unknown <nm_bensaci@esi.dz>
2024-12-21 19:20:19 +0100

    current

--- TC-gestion/src/App.jsx
+++ TC-gestion/src/App.jsx
@@ -1,14 +1,15 @@
 /* eslint-disable no-unused-vars */
 import "./app.css";
+import AddNew from "./pages/AddNew";
 import {RouterProvider, createBrowserRouter, createRoutesFromElements, Route} from "react-router-dom"
 import Editpage from "./pages/Editpage";
 import Signup from "./pages/signup";
-
+import Profile from "./pages/Profile";
 
 const App = () => {
 
   return (
-    <Editpage />
+    <Profile />
   );
 };
 
